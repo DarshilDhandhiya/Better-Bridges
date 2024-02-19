d0e1c2b802f8bb3139ca04adabb354dcbbb9f1a1
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-02-19 18:03:37 +0530

    Done

---
+++ components/NotificationsFeed.tsx
@@ -0,0 +1,44 @@
+import { BsTwitter } from "react-icons/bs";
+import Image from 'next/image'
+
+import useNotifications from "@/hooks/useNotifications";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { useEffect } from "react";
+
+const NotificationsFeed = () => {
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { data: fetchedNotifications = [] } = useNotifications(currentUser?.id);
+
+  useEffect(() => {
+    mutateCurrentUser();
+  }, [mutateCurrentUser]);
+
+  if (fetchedNotifications.length === 0) {
+    return (
+      <div className="text-neutral-600 text-center p-6 text-xl">
+        No notifications
+      </div>
+    )
+  }
+  
+  return ( 
+    <div className="flex flex-col">
+      {fetchedNotifications.map((notification: Record<string, any>) => (
+        <div key={notification.id} className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800">
+
+          <Image
+      src="/5b.png"
+      width={35}
+      height={300}
+      alt="Picture of the author"
+    /> 
+          <p className="text-white">
+            {notification.body}
+          </p>
+        </div>
+        ))}
+    </div>
+   );
+}
+ 
+export default NotificationsFeed;
